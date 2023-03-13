1df01af6edbd0dc9d3d4e62e5e0b7ffb4bf7106f
Dwayna Harris <dwaynaharris@gmail.com>
2023-03-13 00:13:06 -0400

    create functions that use arrays to display messages and create new arrays

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards() {
+    const cards = [["Guadalupe", "Ollie", "Aki"], "surprise"];
+    const tnx = [];
+    for(let i = 0; i <= 2; i++) {
+        tnx.push("Thank you, " + cards[0][i] + ", for the wonderful " + cards[1] + " gift!");
+    }
+    return tnx;
+}
+
+function countDown() {
+    let int = 10;
+    while(int >= 0) {
+        console.log(int);
+        int--;
+    }
+}
